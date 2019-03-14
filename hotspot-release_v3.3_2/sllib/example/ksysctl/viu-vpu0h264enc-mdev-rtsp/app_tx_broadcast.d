eddd459594d14adbf2fb08e90f951951a6ab038a
Acer <742128675@qq.com>
2019-03-14 17:13:37 +0800

    update

---
+++ hotspot-release_v3.3_2/sllib/example/ksysctl/viu-vpu0h264enc-mdev-rtsp/app_tx_broadcast.d
@@ -0,0 +1,10 @@
+app_tx_broadcast.o: \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/example/ksysctl/viu-vpu0h264enc-mdev-rtsp/app_tx_broadcast.c \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/example/ksysctl/viu-vpu0h264enc-mdev-rtsp/app_tx_broadcast.h \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/example/ksysctl/viu-vpu0h264enc-mdev-rtsp/sharemem.h \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_types.h \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_debug.h \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_config.h \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/example/ksysctl/viu-vpu0h264enc-mdev-rtsp/Setting.h \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/example/ksysctl/viu-vpu0h264enc-mdev-rtsp/../version.h \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/example/ksysctl/viu-vpu0h264enc-mdev-rtsp/cfginfo.h
---
+++ hotspot-release_v3.3_2/sllib/example/ksysctl/viu-vpu0h264enc-mdev-rtsp/sl_uart.d
@@ -0,0 +1,7 @@
+sl_uart.o: \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/example/ksysctl/viu-vpu0h264enc-mdev-rtsp/sl_uart.c \
+ sl_uart.h \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_types.h \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_debug.h \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_config.h \
+ /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_error.h
