3debdf206b62d730a6b86e1ae1526f0811d520fd
Acer <742128675@qq.com>
2019-03-14 17:35:35 +0800

    update gitignore

--- hotspot-release_v3.3_2/sllib/example/ksysctl/rtsp-mdev-vpu0-vpp_pv/app_rtp.d
+++
@@ -1,6 +0,0 @@
-app_rtp.o: \
- /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/example/ksysctl/rtsp-mdev-vpu0-vpp_pv/app_rtp.c \
- /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_types.h \
- /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_debug.h \
- /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_config.h \
- /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/example/ksysctl/rtsp-mdev-vpu0-vpp_pv/app_rtp.h
--- hotspot-release_v3.3_2/sllib/example/ksysctl/rtsp-mdev-vpu0-vpp_pv/uart_watchdog.d
+++
@@ -1,8 +0,0 @@
-uart_watchdog.o: \
- /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/example/ksysctl/rtsp-mdev-vpu0-vpp_pv/uart_watchdog.c \
- sl_uart.h \
- /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_types.h \
- /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_debug.h \
- /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_config.h \
- /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/include/sl_error.h \
- /home/ubuntu/Work/8925/test/hotspot-release_v3.3_2/sllib/example/ksysctl/rtsp-mdev-vpu0-vpp_pv/uart_watchdog.h
