ddf2a916185c5e8c39277bf71493222fbf6c5006
Acer <742128675@qq.com>
2019-03-21 18:48:45 +0800

    hsv562 branch

--- hotspot-release_v3.3_2/sllib/example/gpio/main.d
+++
@@ -1,7 +0,0 @@
-main.o: \
- /home/y/work/8925Docs/8925_test/hotspot-release_v1.3/sllib/example/gpio/main.c \
- sl_gpio.h \
- /home/y/work/8925Docs/8925_test/hotspot-release_v1.3/sllib/include/sl_types.h \
- /home/y/work/8925Docs/8925_test/hotspot-release_v1.3/sllib/include/sl_debug.h \
- /home/y/work/8925Docs/8925_test/hotspot-release_v1.3/sllib/include/sl_config.h \
- /home/y/work/8925Docs/8925_test/hotspot-release_v1.3/sllib/include/sl_error.h
