d12cb2c379a2a8ffd8e577c4bfb4c22d3a953123
Acer <742128675@qq.com>
2019-03-22 09:33:14 +0800

    update version.h

--- hotspot-release_v3.3_2/sllib/example/ksysctl/version.h
+++ hotspot-release_v3.3_2/sllib/example/ksysctl/version.h
@@ -3,7 +3,7 @@
 ************************************************************
 *   product   | function                  |   version :
 *   HSV892    | one more                  | 0.0.x
-*   HSV893    | SWITCH control            | 0.1.x
+*   HSV893    | SWITCH control more       | 0.1.x
 *   HSV562    | KVM control + one more    | 2.0.x
 *   HSV592    | KVM control + more one    | 2.1.x
 *   HSV701    | WEB control + distribute  | 1.0.x
@@ -22,16 +22,16 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define PRODUCT_NAME   "HSV592"     //product name
+#define PRODUCT_NAME   "HSV562"     //product name
 #define VER_ID1      "2"          //major version id 
-#define VER_ID2      "1"          //minor version id 
+#define VER_ID2      "0"          //minor version id 
 #define VER_ID3      "1"             //sbuversion id 
-#define BUILDING      "20190219"        //date
-#define CURRTNE_STATE "release" 
+#define BUILDING      "20190221"        //date
+#define CURRTNE_STATE "" 
 
-#define PRINT_VERSION    "HSV592 V2.1.0.190219_R"
-#define WEB_VERSION      "V2.1.1.190219"
-#define OSD_VERSION      "V2.1.1"
+#define PRINT_VERSION    "HSV562 V2.0.1.190221_ALPHA"
+#define WEB_VERSION      "V2.0.1.190221"
+#define OSD_VERSION      "V2.0.1"
 
 
 
