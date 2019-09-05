c9cd13bee8f7b48532e231bff7313bb65600753f
King <742128675@qq.com>
2019-09-05 15:44:42 +0800

    update version to V2.3.2 and debug TX input 32khz audio issue

--- hotspot-release_v3.3_2/sllib/example/ksysctl/version.h
+++ hotspot-release_v3.3_2/sllib/example/ksysctl/version.h
@@ -6,9 +6,10 @@
 *   HSV893    | SWITCH control            | 0.1.x
 *   HSV562    | KVM control + one more    | 2.0.x
 *   HSV592    | KVM control + more one    | 2.1.x
+*	HSV895	  | UART + IR + one more      | 2.2.x
 *   HSV701    | WEB control + distribute  | 1.0.x
 *   HSV761    | WEB control + centralize  | 1.1.x
-*	HSV712	  | KVM control + more to more | 2.2.x
+*	HSV712	  | KVM control + more to more | 2.3.x
 *   HSV900/922  
 *************************************************************
 *   current state |  abridge
@@ -25,14 +26,22 @@
 
 #define PRODUCT_NAME   "HSV712"     //product name
 #define VER_ID1      "2"          //major version id 
-#define VER_ID2      "2"          //minor version id 
-#define VER_ID3      "1"             //subversion id 
-#define BUILDING      "20190816"        //date
-#define CURRTNE_STATE "release" 
+#define VER_ID2      "3"          //minor version id 
+#define VER_ID3      "2"             //subversion id 
+#define BUILDING      "20190905"        //date
+#define CURRTNE_STATE "debug" 
 
-#define PRINT_VERSION    "HSV712 V2.2.1.190816_R"
-#define WEB_VERSION      "V2.2.1.190816"
-#define OSD_VERSION      "V2.2.1"
+#define PRINT_VERSION    "HSV712 V2.3.2.190905_ALPHA"
+#define WEB_VERSION      "V2.3.2.190905"
+#define OSD_VERSION      "V2.3.2"
+
+/*
+ * V2.3.2 : debug TX input 32khz audio issue
+
+
+*/
+
+
 
 
 
